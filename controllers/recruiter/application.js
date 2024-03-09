60f5da0e1cae4e5d43813413996931c9d19c8a86
apni-coding <apnicoding72@gmail.com>
2024-03-09 13:07:44 +0530

    fix error

--- controllers/recruiter/application.js
+++ controllers/recruiter/application.js
@@ -5,22 +5,24 @@ const { INTERNAL_SERVER_ERROR } = require("../../response_messages/errorMessage"
 const { ERROR, SUCCESS } = require("../../response_messages/statusCode");
 
 exports.getJobWithTotalApplication = async (req, res) => {
-  try {
-    const userId = req.userId;
-    const jobDetails = await jobModel.find({ jobOwnerId: userId, isOpening: true });
-    let totalApplicationCount = 0
-    const jobDetailsWithApplicationCount = await Promise.all(jobDetails.map(async (job) => {
-      const applicationCount = await applicationModel.countDocuments({ job: job._id, status: 'pending' });
-      totalApplicationCount += applicationCount;
-      return { ...job.toObject(), totalApplicationCount };
-    }));
-
-    res.status(SUCCESS).json( jobDetailsWithApplicationCount );
-  } catch (error) {
-    console.error(error);
-    res.status(ERROR).json({ error: INTERNAL_SERVER_ERROR });
-  }
-};
+    try {
+      const userId = req.userId;
+      const jobDetails = await jobModel.find({ jobOwnerId: userId, isOpening: true });
+  
+      // Get total application count for all jobs concurrently
+      const applicationCountsPromises = jobDetails.map(async (job) => {
+        const applicationCount = await applicationModel.countDocuments({ job: job._id, status: 'pending' });
+        return { ...job.toObject(), applicationCount };
+      });
+      const jobDetailsWithApplicationCount = await Promise.all(applicationCountsPromises);
+  
+      res.status(SUCCESS).json(jobDetailsWithApplicationCount);
+    } catch (error) {
+      console.error(error);
+      res.status(ERROR).json({ error: INTERNAL_SERVER_ERROR });
+    }
+  };
+  
 
 exports.getApplicationByJobId = async (req, res) => {
     try {
